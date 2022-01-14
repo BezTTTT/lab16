e36cc488a1c0e2000a0ae78c73c2cd06a3ae7e25
BezTTTT <Best_1614@hotmail.com>
2022-01-14 19:58:13 +0700

    lab16_4 completed

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -8,7 +8,7 @@ void shuffle(int &,int &,int &,int &);
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
 	
-	srand(time(0));	
+	srand(time(0));
 	
 	for(int i = 0;i < 10;i++){
 	    shuffle(a,b,c,d);
@@ -16,4 +16,25 @@ int main(){
 	}
 	
 	return 0;
+}
+void shuffle(int& a,int& b,int& c,int& d){
+	int temp = 0;
+	if(rand()%4 == 0)
+	{
+	temp = a;
+	a = b;
+	b=temp;
+	}else if (rand()%4 == 1){
+		temp = b;
+		b = c;
+		c=temp;
+	}else if (rand()%4 == 2){
+		temp = c;
+		c = d;
+		d=temp;
+	}else if(rand()%4 == 3){
+		temp = d;
+		d = a;
+		a=temp;
+	}
 }
